a56518040f51367f06627db0d0b16bcc2b4b5c61
Nathaniel Lee <nlee5082@usc.edu>
2024-12-09 12:04:02 -0800

    Update WorkoutRepository.java

--- backend/src/main/java/csci201/backend/repository/WorkoutRepository.java
+++ backend/src/main/java/csci201/backend/repository/WorkoutRepository.java
@@ -4,7 +4,11 @@ import csci201.backend.entity.Workout;
 import csci201.backend.entity.Workout.WorkoutType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.sql.Timestamp;
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
@@ -14,4 +18,9 @@ public interface WorkoutRepository extends JpaRepository<Workout, Integer> {
     long countByUserUserIdAndWorkoutTimestampBetween(int userId, Timestamp start, Timestamp end);
 
     List<Workout> findTop3ByUserUserIdOrderByWorkoutTimestampDesc(int userId);
+
+    List<Weight> findByUserId(int userId);
+
+    @Query("SELECT w FROM Weight w WHERE w.userId = :userId AND DATE(w.weightTimestamp) = :date")
+    List<Weight> findByUserIdAndDate(@Param("userId") int userId, @Param("date") LocalDate date);
 }
